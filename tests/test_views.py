1e07c6b738d144a6bf4d977c835ea1a4d27523a7
Tobias Frust <t.frust@hzdr.de>
2017-04-18 13:44:49 +0200

    add more tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -16,9 +16,45 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Test case for views."""
+"""Test cases for views."""
+
+import os
 
 from flask import url_for
+
+
+def _invalid_configuration(app):
+    app.config['SHIBBOLETH_REMOTE_APPS'].update(
+        dict(
+            idp=dict(
+                title='Test identity provider'
+            )
+        )
+    )
+
+
+def _invalid__saml_configuration(app):
+    app.config['SHIBBOLETH_REMOTE_APPS'].update(
+        dict(
+            idp=dict(
+                title='Test identity provider',
+                saml_path=os.path.join(os.path.dirname(__file__),
+                                       'data/invalid/')
+            )
+        )
+    )
+
+
+def _valid_configuration(app):
+    app.config['SHIBBOLETH_REMOTE_APPS'].update(
+        dict(
+            idp=dict(
+                title='Test identity provider',
+                saml_path=os.path.join(os.path.dirname(__file__),
+                                       'data/valid/')
+            )
+        )
+    )
 
 
 def test_login(views_fixture):
@@ -30,3 +66,77 @@ def test_login(views_fixture):
             url_for('shibboleth_authenticator.login', remote_app='invalid')
         )
         assert resp.status_code == 404
+
+        # Invalid configuration
+        _invalid_configuration(app)
+        resp = client.get(
+            url_for('shibboleth_authenticator.login', remote_app='idp')
+        )
+        assert resp.status_code == 500
+
+        # Invalid configuration of python3-saml
+        _invalid__saml_configuration(app)
+        resp = client.get(
+            url_for('shibboleth_authenticator.metadata', remote_app='idp')
+        )
+        assert resp.status_code == 500
+
+
+def test_authorized(views_fixture):
+    """Test authorized view."""
+    app = views_fixture
+
+    with app.test_client() as client:
+        # Invalid remote
+        resp = client.get(
+            url_for('shibboleth_authenticator.authorized',
+                    remote_app='invalid')
+        )
+        assert resp.status_code == 404
+
+        # Invalid configuration
+        _invalid_configuration(app)
+        resp = client.get(
+            url_for('shibboleth_authenticator.authorized', remote_app='idp')
+        )
+        assert resp.status_code == 500
+
+        # Invalid configuration of python3-saml
+        _invalid__saml_configuration(app)
+        resp = client.get(
+            url_for('shibboleth_authenticator.metadata', remote_app='idp')
+        )
+        assert resp.status_code == 500
+
+
+def test_metadata(views_fixture):
+    """Test metadata view."""
+    app = views_fixture
+    with app.test_client() as client:
+        # Invalid remote application
+        resp = client.get(
+            url_for('shibboleth_authenticator.metadata', remote_app='invalid')
+        )
+        assert resp.status_code == 404
+
+        # Invalid configuration
+        _invalid_configuration(app)
+        resp = client.get(
+            url_for('shibboleth_authenticator.metadata', remote_app='idp')
+        )
+        assert resp.status_code == 500
+
+        # Valid configuration
+        _valid_configuration(app)
+        resp = client.get(
+            url_for('shibboleth_authenticator.metadata', remote_app='idp')
+        )
+        assert resp.status_code == 200
+        assert resp.headers['Content-Type'] in ['text/xml', 'application/xml']
+
+        # Invalid configuration of python3-saml
+        _invalid__saml_configuration(app)
+        resp = client.get(
+            url_for('shibboleth_authenticator.metadata', remote_app='idp')
+        )
+        assert resp.status_code == 500
